e73bde11d9dd290c8024220904254e51e424f727
Arviixzuh <arviixzuh@gmail.com>
2026-04-02 04:50:47 -0400

    feat: ✨ update ApartmentModel to include expenses, reservations, and promotion fields

--- backend/src/modules/apartment/domain/models/apartment.model.ts
+++ backend/src/modules/apartment/domain/models/apartment.model.ts
@@ -1,15 +1,25 @@
 import { ApartmentStatus } from 'generated/prisma/enums'
 import { ApartmentImageModel } from './apartment-image.model'
+import { ReservationModel } from '@/modules/reservation/domain/models/reservation.model' // Ajusta los paths
+import { ExpenseModel } from '@/modules/expense/domain/models/expense.model'
+import { Decimal } from '@prisma/client/runtime/client'
+import { PromotionModel } from '@/modules/promotion/domain/models/promotion.model'
 
 export class ApartmentModel {
   id: number
   floor: number
   number: string
-  images?: ApartmentImageModel[] | null
   status: ApartmentStatus
   bedrooms: number
   bathrooms: number
-  createdAt?: Date | null
+  pricePerDay: Decimal
+  promotion?: PromotionModel | null
   squareMeters?: number | null
-  updatedAt?: Date | null
+  images?: ApartmentImageModel[] | null
+  expenses?: ExpenseModel[] | null
+  reservations?: ReservationModel[] | null
+  createdAt: Date
+  updatedAt: Date
+  deletedAt?: Date | null
+  promotionId?: number | null
 }
