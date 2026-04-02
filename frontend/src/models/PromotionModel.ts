043c1917aa18124a4c6972e8a8f458c18d9d4648
Arviixzuh <arviixzuh@gmail.com>
2026-04-02 11:05:28 -0400

    chore: ✨ apply formatting and fix linting errors across the codebase

--- frontend/src/models/PromotionModel.ts
+++ frontend/src/models/PromotionModel.ts
@@ -1,25 +1,25 @@
-import { ApartmentModel } from "./ApartmentModel";
+import { ApartmentModel } from './ApartmentModel'
 import { IPaginationFilter } from '@/api/interfaces'
 
 export enum PromotionType {
-    FIXED = 'FIXED',
-    PERCENTAGE = 'PERCENTAGE',
+  FIXED = 'FIXED',
+  PERCENTAGE = 'PERCENTAGE',
 }
 
 export interface PromotionModel {
-    id: number;
-    name: string;
-    type: PromotionType;
-    value: number | string;
-    createdAt: Date;
-    updatedAt: Date;
-    description: string | null;
-    apartments?: ApartmentModel[];
+  id: number
+  name: string
+  type: PromotionType
+  value: number | string
+  createdAt: Date
+  updatedAt: Date
+  description: string | null
+  apartments?: ApartmentModel[]
 }
 
 export interface IPromotionFilter extends IPaginationFilter {
-    name?: string
-    search?: string
-    toDate?: string
-    fromDate?: string
+  name?: string
+  search?: string
+  toDate?: string
+  fromDate?: string
 }
--- frontend/src/services/promotion/index.ts
+++ frontend/src/services/promotion/index.ts
@@ -8,44 +8,47 @@ import { DELETE_PROMOTION } from './graphql/deletePromotionMutation'
 import { PromotionModel, IPromotionFilter } from '@/models/PromotionModel'
 
 export const promotionService = {
-    get: async (id: number): Promise<PromotionModel | null> => {
-        const { data } = await apolloClient.query<{ findOne: PromotionModel }>({
-            query: FIND_PROMOTION,
-            variables: { id },
-        })
-        return data?.findOne || null
-    },
-    getAll: async (filters: IPromotionFilter): Promise<IPageResponse<PromotionModel> | null> => {
-        const { data } = await apolloClient.query<{ findPromotions: IPageResponse<PromotionModel> }>({
-            query: FIND_PROMOTIONS,
-            variables: { filters },
-        })
-        return data?.findPromotions || null
-    },
-    create: async (payload: Partial<PromotionModel>): Promise<PromotionModel | undefined> => {
-        const { data } = await apolloClient.mutate<{ createPromotion: PromotionModel }>({
-            mutation: CREATE_PROMOTION,
-            variables: {
-                data: payload,
-            },
-        })
-        return data?.createPromotion
-    },
-    update: async (id: number, payload: Partial<PromotionModel>): Promise<PromotionModel | undefined> => {
-        const { data } = await apolloClient.mutate<{ updatePromotion: PromotionModel }>({
-            mutation: UPDATE_PROMOTION,
-            variables: {
-                id,
-                data: payload,
-            },
-        })
-        return data?.updatePromotion
-    },
-    delete: async (id: number): Promise<boolean> => {
-        const { data } = await apolloClient.mutate<{ deletePromotion: boolean }>({
-            mutation: DELETE_PROMOTION,
-            variables: { id },
-        })
-        return !!data?.deletePromotion
-    },
+  get: async (id: number): Promise<PromotionModel | null> => {
+    const { data } = await apolloClient.query<{ findOne: PromotionModel }>({
+      query: FIND_PROMOTION,
+      variables: { id },
+    })
+    return data?.findOne || null
+  },
+  getAll: async (filters: IPromotionFilter): Promise<IPageResponse<PromotionModel> | null> => {
+    const { data } = await apolloClient.query<{ findPromotions: IPageResponse<PromotionModel> }>({
+      query: FIND_PROMOTIONS,
+      variables: { filters },
+    })
+    return data?.findPromotions || null
+  },
+  create: async (payload: Partial<PromotionModel>): Promise<PromotionModel | undefined> => {
+    const { data } = await apolloClient.mutate<{ createPromotion: PromotionModel }>({
+      mutation: CREATE_PROMOTION,
+      variables: {
+        data: payload,
+      },
+    })
+    return data?.createPromotion
+  },
+  update: async (
+    id: number,
+    payload: Partial<PromotionModel>,
+  ): Promise<PromotionModel | undefined> => {
+    const { data } = await apolloClient.mutate<{ updatePromotion: PromotionModel }>({
+      mutation: UPDATE_PROMOTION,
+      variables: {
+        id,
+        data: payload,
+      },
+    })
+    return data?.updatePromotion
+  },
+  delete: async (id: number): Promise<boolean> => {
+    const { data } = await apolloClient.mutate<{ deletePromotion: boolean }>({
+      mutation: DELETE_PROMOTION,
+      variables: { id },
+    })
+    return !!data?.deletePromotion
+  },
 }
