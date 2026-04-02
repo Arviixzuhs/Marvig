610c04a7e3b0797483a7d6b0a912ceedbe945516
Arviixzuh <arviixzuh@gmail.com>
2026-04-02 04:57:42 -0400

    feat: ✨ add ReservationApplicationModule to PaymentInfrastructureModule imports

--- backend/src/modules/payment/infrastructure/payment.infrastructure.module.ts
+++ backend/src/modules/payment/infrastructure/payment.infrastructure.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { PaymentResolver } from '@/modules/payment/infrastructure/graphql/resolvers/payment.resolver'
 import { StripeController } from './controllers/stripe.controller'
 import { PaymentApplicationModule } from '@/modules/payment/application/payment.application.module'
+import { ReservationApplicationModule } from '@/modules/reservation/application/reservation.application.module'
 
 @Module({
-  imports: [PaymentApplicationModule],
+  imports: [PaymentApplicationModule, ReservationApplicationModule],
   controllers: [StripeController],
   providers: [PaymentResolver],
   exports: [],
