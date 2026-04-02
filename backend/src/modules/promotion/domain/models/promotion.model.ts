1493aa03668c4c2da34783b64d963496163e7467
Arviixzuh <arviixzuh@gmail.com>
2026-04-02 04:57:58 -0400

    feat: ✨ add type and value properties to PromotionModel

--- backend/src/modules/promotion/domain/models/promotion.model.ts
+++ backend/src/modules/promotion/domain/models/promotion.model.ts
@@ -1,8 +1,13 @@
+import { Decimal } from '@prisma/client/runtime/client'
+import { PromotionType } from 'generated/prisma/enums'
+
 export class PromotionModel {
   id: number
   name: string
   description?: string | null
   isDeleted?: boolean | null
+  type: PromotionType
+  value: Decimal
   createdAt?: Date | null
   updatedAt?: Date | null
   deletedAt?: Date | null
