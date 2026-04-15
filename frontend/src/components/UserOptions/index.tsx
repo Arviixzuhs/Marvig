dee92b3efff4a75bc0cc923bac75f4e0436f4bfd
Arviixzuh <arviixzuh@gmail.com>
2026-04-14 23:03:08 -0400

    refactor: 👤 update NavbarUserOptions to use Redux global state for user detection and conditionally hide the Dashboard link when already in the admin area

--- frontend/src/components/UserOptions/index.tsx
+++ frontend/src/components/UserOptions/index.tsx
@@ -1,16 +1,17 @@
-import Cookies from 'js-cookie'
 import { logOut } from '@/utils/logOut'
 import { RootState } from '@/store'
 import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 import { ExternalLink } from 'lucide-react'
 import { Avatar, Dropdown, DropdownMenu, DropdownItem, DropdownTrigger, Link } from '@heroui/react'
 
 export const NavbarUserOptions = () => {
   const user = useSelector((state: RootState) => state.user)
 
-  const token = Cookies.get('isLoggedIn') || localStorage.getItem('token')
+  const location = useLocation()
+  const isInAdmin = location.pathname.includes('/admin')
 
-  if (token) {
+  if (user) {
     return (
       <Dropdown>
         <DropdownTrigger>
@@ -27,13 +28,17 @@ export const NavbarUserOptions = () => {
             <p className='font-semibold'>Registrado como</p>
             <p className='font-semibold'>{user?.email || 'Usuario'}</p>
           </DropdownItem>
-          {token && (
-            <DropdownItem key='dashboard'>
-              <Link href='/admin/dashboard' target='_blank' className='flex gap-2'>
-                Dashboard
-                <ExternalLink size={15} />
-              </Link>
-            </DropdownItem>
+          {user && !isInAdmin ? (
+            <>
+              <DropdownItem key='dashboard'>
+                <Link href='/admin/dashboard' target='_blank' className='flex gap-2'>
+                  Dashboard
+                  <ExternalLink size={15} />
+                </Link>
+              </DropdownItem>
+            </>
+          ) : (
+            <></>
           )}
           <DropdownItem
             key='logout'
