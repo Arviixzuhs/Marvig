1c66ba4d96e4e323e22718399fd4cf9ddd21a24a
Arviixzuh <arviixzuh@gmail.com>
2026-04-14 22:56:34 -0400

    refactor: 🛠️ ensure CurrentUser decorator uses GqlExecutionContext for consistent user retrieval in GraphQL contexts

--- backend/src/common/decorators/current-user.decorator.ts
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -1,11 +1,14 @@
 import { User } from '@/interfaces/user.interface'
 import { GqlExecutionContext } from '@nestjs/graphql'
-import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common'
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext): User => {
     const ctx = GqlExecutionContext.create(context)
     const req = ctx.getContext().req
+    if (!req.user) {
+      throw new UnauthorizedException('Usuario no identificado')
+    }
     return req.user
   },
 )
