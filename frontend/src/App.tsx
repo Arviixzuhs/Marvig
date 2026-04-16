4e09529bf06f4863408eeb1e98c7654a2a53feb3
Arviixzuh <arviixzuh@gmail.com>
2026-04-15 21:46:37 -0400

    feat: 🩺 fix typo in AdminExpensePage import and update its corresponding route in the App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { RegisterPage } from '@/modules/auth/pages/Register'
 import { AdminUserPage } from '@/modules/admin/pages/users'
 import { Route, Routes } from 'react-router-dom'
 import { AdminPaymentPage } from '@/modules/admin/pages/payments'
-import { AdminExepensePage } from '@/modules/admin/pages/expenses'
+import { AdminExpensePage } from '@/modules/admin/pages/expenses'
 import { AdminEmployeePage } from '@/modules/admin/pages/employees'
 import { AdminPromotionPage } from '@/modules/admin/pages/promotion'
 import { AdminApartmentPage } from '@/modules/admin/pages/apartments'
@@ -23,7 +23,7 @@ function App() {
         <Route element={<AdminLayout />} path='/admin'>
           <Route element={<AdminUserPage />} path='users' />
           <Route element={<AdminPaymentPage />} path='payments' />
-          <Route element={<AdminExepensePage />} path='expenses' />
+          <Route element={<AdminExpensePage />} path='expenses' />
           <Route element={<AdminEmployeePage />} path='employees' />
           <Route element={<AdminDashboardPage />} path='dashboard' />
           <Route element={<AdminApartmentPage />} path='apartments' />
