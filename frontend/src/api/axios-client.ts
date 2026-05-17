6db19b0aa06ecae99fc62bc661d907a2bff58d84
Arviixzuh <arviixzuh@gmail.com>
2026-05-17 15:53:35 -0400

    feat: ✨ add error handling interceptor for API responses to improve user feedback

--- frontend/src/api/axios-client.ts
+++ frontend/src/api/axios-client.ts
@@ -1,6 +1,7 @@
 import { ENV } from '@/constants'
 import axios from 'axios'
 import qs from 'qs'
+import toast from 'react-hot-toast'
 
 export const authApi = axios.create({
   baseURL: ENV['VITE_SERVER_API'],
@@ -20,3 +21,42 @@ export const api = axios.create({
       arrayFormat: 'repeat',
     }),
 })
+
+// Interceptor para manejar errores y mostrarlos
+const handleError = (error: any) => {
+  if (error.response?.status === 403) {
+    window.location.href = '/'
+  }
+
+  if (error && error.status !== 404) {
+    let errorMessage = ''
+
+    if (error.response?.data?.message) {
+      errorMessage = error.response?.data?.message
+    }
+
+    if (error?.response?.data?.errors && error?.response?.data?.errors[0]) {
+      errorMessage = error.response?.data?.errors[0]?.message || error.response?.data?.errors[0]
+    }
+
+    if (errorMessage?.trim() !== '') {
+      toast.error(errorMessage)
+    }
+  }
+}
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    handleError(error)
+    return Promise.reject(error)
+  },
+)
+
+authApi.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    handleError(error)
+    return Promise.reject(error)
+  },
+)
