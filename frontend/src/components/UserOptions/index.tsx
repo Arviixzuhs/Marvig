286ab24b472764639668b390e5151835bf8282ce
Arviixzuh <arviixzuh@gmail.com>
2026-05-17 18:39:29 -0400

    feat: ✨ refactor NavbarUserOptions to use navigate for routing and improve dropdown item structure

--- frontend/src/components/UserOptions/index.tsx
+++ frontend/src/components/UserOptions/index.tsx
@@ -2,14 +2,15 @@ import { logOut } from '@/utils/logOut'
 import { UserRole } from '@/models/UserModel'
 import { RootState } from '@/store'
 import { useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
-import { ExternalLink } from 'lucide-react'
-import { Avatar, Dropdown, DropdownMenu, DropdownItem, DropdownTrigger, Link } from '@heroui/react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { ExternalLink, Cog, DoorOpen } from 'lucide-react'
+import { Avatar, Dropdown, DropdownMenu, DropdownItem, DropdownTrigger } from '@heroui/react'
 
 export const NavbarUserOptions = () => {
   const user = useSelector((state: RootState) => state.user)
   const location = useLocation()
   const isInAdmin = location.pathname.includes('/admin')
+  const navigate = useNavigate()
 
   if (user) {
     return (
@@ -24,20 +25,29 @@ export const NavbarUserOptions = () => {
           </DropdownItem>
           {user.role === UserRole.ADMIN && !isInAdmin ? (
             <>
-              <DropdownItem key='dashboard'>
-                <Link href='/admin/dashboard' target='_blank' className='flex gap-2'>
-                  Dashboard
-                  <ExternalLink size={15} />
-                </Link>
+              <DropdownItem
+                key='dashboard'
+                startContent={<ExternalLink size={15} />}
+                onPress={() => navigate('/admin/dashboard')}
+              >
+                Dashboard
               </DropdownItem>
             </>
           ) : (
             <></>
           )}
           <DropdownItem
+            key='dashboard'
+            startContent={<Cog size={15} />}
+            onPress={() => navigate('/config')}
+          >
+            Confuguración
+          </DropdownItem>
+          <DropdownItem
             key='logout'
             className='text-danger'
             color='danger'
+            startContent={<DoorOpen size={15} />}
             onClick={() => logOut()}
           >
             Cerrar sesión
