1db9fe0b774e9abb819b4f6fd6eafd9b2827f43f
Arviixzuh <arviixzuh@gmail.com>
2026-05-17 22:48:55 -0400

    feat: 🥞 implement authentication and admin middleware for route protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,31 +5,38 @@ import { LandingPage } from '@/modules/landing'
 import { AdminLayout } from '@/modules/admin/layout'
 import { RegisterPage } from '@/modules/auth/pages/Register'
 import { AdminUserPage } from '@/modules/admin/pages/users'
-import { Route, Routes } from 'react-router-dom'
 import { ApartmentPage } from '@/modules/apartments/pages/apartment'
 import { ApartmentsPage } from '@/modules/apartments/pages/apartments'
+import { AuthMiddleware } from './middlewares/AuthMiddleware'
 import { AdminReportsPage } from '@/modules/admin/pages/reports'
 import { AdminPaymentPage } from '@/modules/admin/pages/payments'
 import { AdminExpensePage } from '@/modules/admin/pages/expenses'
 import { AdminEmployeePage } from '@/modules/admin/pages/employees'
+import { IsAdminMiddleware } from './modules/admin/middlewares/IsAdminMiddleware'
 import { AdminPromotionPage } from '@/modules/admin/pages/promotion'
 import { AdminApartmentPage } from '@/modules/admin/pages/apartments'
 import { AdminDashboardPage } from '@/modules/admin/pages/dashboard'
 import { AdminReservationPage } from '@/modules/admin/pages/reservations'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoadCurrentUserMiddleware } from '@/middlewares/LoadCurrentUserMiddleware'
 
 function App() {
   return (
     <Routes>
+      <Route path='*' element={<Navigate to='/' />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route element={<LoadCurrentUserMiddleware />}>
         <Route element={<UserLayout />}>
-          <Route element={<ConfigPage />} path='/config' />
+          <Route element={<AuthMiddleware />}>
+            <Route element={<ConfigPage />} path='/config' />
+          </Route>
           <Route element={<LandingPage />} path='/' />
           <Route element={<ApartmentPage />} path='/apartment/:apartmentId' />
           <Route element={<ApartmentsPage />} path='/apartments' />
         </Route>
+      </Route>
+      <Route element={<IsAdminMiddleware />}>
         <Route element={<AdminLayout />} path='/admin'>
           <Route element={<AdminUserPage />} path='users' />
           <Route element={<AdminReportsPage />} path='reports' />
