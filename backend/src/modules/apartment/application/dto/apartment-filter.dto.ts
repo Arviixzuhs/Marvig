d2806741bacc8401e1c4b1bcc625d6d4b83a7cdd
Arviixzuh <arviixzuh@gmail.com>
2026-05-19 17:21:27 -0400

    refactor: 🧹 remove unnecessary GraphQL InputType decorator from ApartmentFilterDto

--- backend/src/modules/apartment/application/dto/apartment-filter.dto.ts
+++ backend/src/modules/apartment/application/dto/apartment-filter.dto.ts
@@ -1,8 +1,6 @@
-import { InputType } from '@nestjs/graphql'
 import { ApartmentStatusEnum } from '@/modules/apartment/domain/enums/apartment-status.enum'
 import { PaginationFilterDto } from '@/common/dto/pagination-filter.dto'
 
-@InputType()
 export class ApartmentFilterDto extends PaginationFilterDto {
   ids?: number[]
   floor?: number
