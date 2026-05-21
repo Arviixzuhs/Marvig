ae25e35a01951a477cbdc63152ec79de6a970c53
Arviixzuh <arviixzuh@gmail.com>
2026-05-20 22:15:50 -0400

    feat: ✨ add payment property to CreateReservationDto

--- backend/src/modules/reservation/application/dto/create-reservation.dto.ts
+++ backend/src/modules/reservation/application/dto/create-reservation.dto.ts
@@ -1,9 +1,11 @@
 import { RentalType } from '@/modules/reservation/domain/enums/rental-type.enum'
 import { ReservationStatus } from '@/modules/reservation/domain/enums/reservation-status.enum'
+import { ReservationPaymentDto } from './reservation-payment.dto'
 
 export class CreateReservationDto {
   type: RentalType
   status: ReservationStatus
+  payment: ReservationPaymentDto
   endDate: string
   startDate: string
   totalPrice: number
