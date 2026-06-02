702adb9052f4b24b49018ec4d2386a2f249090f4
Arviixzuh <arviixzuh@gmail.com>
2026-06-01 21:34:01 -0400

    feat: ✨ add status filter to PaymentFilterDto and PaymentFilterInput, and update findPayments method to include status filtering

--- backend/src/modules/payment/application/dto/payment-filter.dto.ts
+++ backend/src/modules/payment/application/dto/payment-filter.dto.ts
@@ -1,7 +1,9 @@
+import { PaymentStatus } from '@/modules/payment/domain/enums/payment-status.enum'
 import { PaginationFilterDto } from '@/common/dto/pagination-filter.dto'
 
 export class PaymentFilterDto extends PaginationFilterDto {
   toDate?: string
+  status?: PaymentStatus
   search?: string
   fromDate?: string
   reservationId?: number
