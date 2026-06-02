6145d31ca5208f8fab030837edd9d0abc9e5b7bf
Arviixzuh <arviixzuh@gmail.com>
2026-06-02 02:46:44 -0400

    feat: ✨ refactor AdminPromotionPage to use Apollo Client for data fetching and remove Redux logic

--- frontend/src/modules/admin/pages/promotion/index.tsx
+++ frontend/src/modules/admin/pages/promotion/index.tsx
@@ -1,69 +1,59 @@
-import React from 'react'
 import toast from 'react-hot-toast'
 import { AppTable } from '@/components/AppTable'
+import { useQuery } from '@apollo/client/react'
 import { RootState } from '@/store'
 import { useDebounce } from 'use-debounce'
+import { useSelector } from 'react-redux'
+import { useTablePage } from '@/hooks/useTablePage'
+import { IPageResponse } from '@/api/interfaces'
+import { PromotionModel } from '@/models/PromotionModel'
 import { AppTableActions } from '@/components/AppTable/interfaces/appTable'
+import { FIND_PROMOTIONS } from '@/services/promotion/graphql/findPromotionsQuery'
 import { promotionService } from '@/services/promotion'
-import { useDispatch, useSelector } from 'react-redux'
 import { tableColumns, modalInputs } from './data'
-import {
-  addItem,
-  updateItem,
-  deleteItem,
-  setTableData,
-  setModalInputs,
-  setTableColumns,
-} from '@/features/appTableSlice'
 
 export const AdminPromotionPage = () => {
   const table = useSelector((state: RootState) => state.appTable)
-  const dispatch = useDispatch()
   const [debounceValue] = useDebounce(table.filterValue, 100)
+  useTablePage({ tableColumns, modalInputs })
 
-  const loadData = async () => {
-    try {
-      const response = await promotionService.getAll({
-        page: table.currentPage,
-        search: debounceValue,
-        pageSize: table.rowsPerPage,
-      })
-      if (!response) return
-
-      dispatch(setTableData(response))
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  React.useEffect(() => {
-    loadData()
-  }, [debounceValue, table.currentPage, table.rowsPerPage])
-
-  React.useEffect(() => {
-    dispatch(setModalInputs(modalInputs))
-    dispatch(setTableColumns(tableColumns))
-  }, [])
+  const { data, refetch } = useQuery<{ findPromotions: IPageResponse<PromotionModel> }>(
+    FIND_PROMOTIONS,
+    {
+      variables: {
+        filters: {
+          page: table.currentPage,
+          search: debounceValue,
+          pageSize: table.rowsPerPage,
+        },
+      },
+    },
+  )
 
   const tableActions: AppTableActions = {
     create: async () => {
-      const response = await promotionService.create(table.formData)
-      dispatch(addItem(response))
+      await promotionService.create(table.formData)
+      await refetch()
       toast.success('Promoción creada correctamente')
     },
     delete: async () => {
       await promotionService.delete(table.currentItemToDelete)
-      dispatch(deleteItem(table.currentItemToDelete))
+      await refetch()
       toast.success('Promoción eliminada correctamente')
     },
     update: async () => {
       await promotionService.update(table.currentItemToUpdate, table.formData)
-      dispatch(updateItem({ id: table.currentItemToUpdate, newData: table.formData }))
+      await refetch()
       toast.success('Promoción actualizada correctamente')
     },
   }
 
   return (
-    <AppTable tableActions={tableActions} searchbarPlaceholder='Buscar promoción por nombre...' />
+    <AppTable
+      totalPages={data?.findPromotions.totalPages}
+      tableContent={data?.findPromotions.content || []}
+      tableActions={tableActions}
+      searchbarPlaceholder='Buscar promoción por nombre...'
+    />
   )
 }
