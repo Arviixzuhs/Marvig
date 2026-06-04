6774b276692175bb85dc98023baa3ca8d9099f9e
Arviixzuh <arviixzuh@gmail.com>
2026-06-03 23:48:10 -0400

    refactor: 🛠️ remove date management from checkout slice and associated actions

--- frontend/src/features/checkoutSlice.ts
+++ frontend/src/features/checkoutSlice.ts
@@ -2,10 +2,6 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface AppTableInterface {
   formData: Record<string, string | unknown>
-  date: {
-    start: Date | null
-    end: Date | null
-  }
   nights: number
 }
 
@@ -13,10 +9,6 @@ export const manageCheckoutSlice = createSlice({
   name: 'checkout',
   initialState: {
     formData: {},
-    date: {
-      start: null,
-      end: null,
-    },
     nights: 0,
   } as AppTableInterface,
   reducers: {
@@ -29,15 +21,10 @@ export const manageCheckoutSlice = createSlice({
         state.formData[name] = value
       }
     },
-    setDate: (state, action: PayloadAction<{ start: Date | null; end: Date | null }>) => {
-      const { start, end } = action.payload
-      state.date.start = start
-      state.date.end = end
-    },
     setNights: (state, action: PayloadAction<number>) => {
       state.nights = action.payload
     },
   },
 })
 
-export const { setCheckoutFormData, setDate, setNights } = manageCheckoutSlice.actions
+export const { setCheckoutFormData, setNights } = manageCheckoutSlice.actions
