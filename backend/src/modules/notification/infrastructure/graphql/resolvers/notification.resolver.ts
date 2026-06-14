5948d444754c66da6ed616f123cab6629c8a6855
Arviixzuh <arviixzuh@gmail.com>
2026-06-13 22:29:33 -0400

    feat: ✨ add MarkNotificationsAsRead use case and integrate with Notification resolver

--- backend/src/modules/notification/infrastructure/graphql/resolvers/notification.resolver.ts
+++ backend/src/modules/notification/infrastructure/graphql/resolvers/notification.resolver.ts
@@ -1,17 +1,21 @@
+import { User } from '@/interfaces/user.interface'
 import { UserRole } from '@/common/enums/user-role.enum'
+import { CurrentUser } from '@/common/decorators/current-user.decorator'
 import { RequiredRole } from '@/common/decorators/required-role.decorator'
 import { NotificationType } from '@/modules/notification/infrastructure/graphql/types/notification.type'
 import { NotificationPageType } from '@/modules/notification/infrastructure/graphql/types/notification-page.type'
-import { Args, Query, Resolver } from '@nestjs/graphql'
 import { NotificationFilterInput } from '@/modules/notification/infrastructure/graphql/inputs/notification-filter.input'
 import { FindNotificationsUseCase } from '@/modules/notification/application/usecases/find-notifications.usecase'
+import { MarkNotificationsAsReadUseCase } from '@/modules/notification/application/usecases/mark-notifications-as-read.usecase'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { GetUnreadNotificationsCountUseCase } from '@/modules/notification/application/usecases/get-unread-notifications-count.usecase'
 
 @Resolver(() => NotificationType)
 export class NotificationResolver {
   constructor(
     private readonly findNotificationsUseCase: FindNotificationsUseCase,
-    private readonly getUnreadNotificationsCountUseCase: GetUnreadNotificationsCountUseCase
+    private readonly markNotificationsAsReadUseCase: MarkNotificationsAsReadUseCase,
+    private readonly getUnreadNotificationsCountUseCase: GetUnreadNotificationsCountUseCase,
   ) {}
 
   @Query(() => NotificationPageType)
@@ -26,5 +30,11 @@ export class NotificationResolver {
   @RequiredRole(UserRole.ADMIN)
   getUnreadNotificationsCount(): Promise<number> {
     return this.getUnreadNotificationsCountUseCase.execute()
+  }
+
+  @Mutation(() => String)
+  async markNotificationsAsRead(@CurrentUser() user: User): Promise<String> {
+    await this.markNotificationsAsReadUseCase.execute(user.userId)
+    return 'Notificaciones leidas correctamente'
   }
 }
