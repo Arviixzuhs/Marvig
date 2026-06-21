44bf84ecf36b9ab14a2c8d75a27a179dd7e3eea6
Arviixzuh <arviixzuh@gmail.com>
2026-06-21 17:34:47 -0400

    feat: 🌱 add nights state to CalendarContext and calculate total days based on selected date range

--- frontend/src/context/calendarContext.tsx
+++ frontend/src/context/calendarContext.tsx
@@ -1,8 +1,10 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react'
 import { DateValue, RangeValue } from '@heroui/react'
+import { getLocalTimeZone } from '@internationalized/date'
 
 interface CalendarContextType {
   date: RangeValue<DateValue> | null | undefined
+  nights: number
   setDate: React.Dispatch<React.SetStateAction<RangeValue<DateValue> | null | undefined>>
   refresh: boolean
   refreshCalendar: () => void
@@ -13,13 +15,22 @@ const CalendarContext = createContext<CalendarContextType | undefined>(undefined
 export const CalendarProvider = ({ children }: { children: ReactNode }) => {
   const [date, setDate] = useState<RangeValue<DateValue> | null | undefined>(null)
   const [refresh, setRefress] = useState<boolean>(false)
+  const [nights, setNights] = useState<number>(0)
+
+  const startMs = date?.start.toDate(getLocalTimeZone()).getTime() ?? 0
+  const endMs = date?.end.toDate(getLocalTimeZone()).getTime() ?? 0
+  const totalDays = date ? Math.round((endMs - startMs) / (1000 * 60 * 60 * 24)) : 0
+
+  React.useEffect(() => {
+    setNights(totalDays)
+  }, [totalDays])
 
   const refreshCalendar = () => {
     setRefress(!refresh)
   }
 
   return (
-    <CalendarContext.Provider value={{ date, setDate, refreshCalendar, refresh }}>
+    <CalendarContext.Provider value={{ date, nights, setDate, refreshCalendar, refresh }}>
       {children}
     </CalendarContext.Provider>
   )
