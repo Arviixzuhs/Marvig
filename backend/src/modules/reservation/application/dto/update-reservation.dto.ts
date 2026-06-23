251ad7492dce9ade045bd16e788e3f03d609189b
Arviixzuh <arviixzuh@gmail.com>
2026-06-23 11:07:04 -0400

    feat: ✨ enhance UpdateReservationInput and UpdateReservationDto with additional fields for client details and validation

--- backend/src/modules/reservation/application/dto/update-reservation.dto.ts
+++ backend/src/modules/reservation/application/dto/update-reservation.dto.ts
@@ -1,14 +1,12 @@
-import { RentalType } from '@/modules/reservation/domain/enums/rental-type.enum'
 import { ReservationStatus } from '@/modules/reservation/domain/enums/reservation-status.enum'
 
 export class UpdateReservationDto {
-  type?: RentalType
   status?: ReservationStatus
+  persons?: number
   endDate?: string
   startDate?: string
   totalPrice?: number
   clientName?: string
   clientEmail?: string
   clientPhone?: string
-  apartmentIds?: number[]
 }
