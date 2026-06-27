800cb5193aefe3e7a4c116c7586feb5dcf4139d3
Arviixzuh <arviixzuh@gmail.com>
2026-06-27 19:33:14 -0400

    feat: 🌱 integrate useIsMobile hook for responsive

--- frontend/src/components/AppTable/components/FilterByDate.tsx
+++ frontend/src/components/AppTable/components/FilterByDate.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { RootState } from '@/store'
 import { useLocation } from 'react-router-dom'
+import { useIsMobile } from '@/hooks/useMobile'
+import { I18nProvider } from '@react-aria/i18n'
 import { Calendar, Trash } from 'lucide-react'
 import { CalendarDate, parseDate } from '@internationalized/date'
 import { DateFilterPeriodButtons } from './DateFilterPeriodButtons'
@@ -15,12 +17,12 @@ import {
   type DateValue,
   type RangeValue,
 } from '@heroui/react'
-import { I18nProvider } from '@react-aria/i18n'
 
 export const FilterByDatePicker = () => {
   const dispatch = useDispatch()
   const table = useSelector((state: RootState) => state.appTable)
   const location = useLocation()
+  const isMobile = useIsMobile()
 
   React.useEffect(() => {
     clearFilter()
@@ -68,50 +70,53 @@ export const FilterByDatePicker = () => {
   }
 
   return (
-    <Popover placement='bottom-start'>
-      <PopoverTrigger>
-        <Button
-          variant='flat'
-          radius='lg'
-          startContent={<Calendar size={18} className='text-xl text-default-500' />}
-        >
-          Filtrar fechas
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className='p-0'>
-        <div className='flex flex-col gap-4 p-4 border border-default-100 rounded-xl bg-content1 shadow-md'>
-          <span className='text-xs font-semibold text-default-500 tracking-wider'>
-            Filtrar por fecha
-          </span>
-          <div className='flex flex-col gap-4'>
-            <div className='flex gap-2 items-center'>
-              <div className='w-full'>
-                <I18nProvider locale='es'>
-                  <DateRangePicker
-                    radius='sm'
-                    value={getDateValue()}
-                    onChange={handleChangeDate}
-                    aria-label='Fecha'
+    <div>
+      <Popover placement='bottom-start'>
+        <PopoverTrigger>
+          <Button
+            radius='lg'
+            variant='flat'
+            isIconOnly={isMobile}
+            startContent={<Calendar size={18} className='text-xl text-default-500' />}
+          >
+            {!isMobile ? 'Filtrar fechas' : ''}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className='p-0'>
+          <div className='flex flex-col gap-4 p-4 border border-default-100 rounded-xl bg-content1 shadow-md'>
+            <span className='text-xs font-semibold text-default-500 tracking-wider'>
+              Filtrar por fecha
+            </span>
+            <div className='flex flex-col gap-4'>
+              <div className='flex gap-2 items-center'>
+                <div className='w-full'>
+                  <I18nProvider locale='es'>
+                    <DateRangePicker
+                      radius='sm'
+                      value={getDateValue()}
+                      onChange={handleChangeDate}
+                      aria-label='Fecha'
+                    />
+                  </I18nProvider>
+                </div>
+                <Button
+                  onPress={clearFilter}
+                  isIconOnly
+                  variant='flat'
+                  radius='sm'
+                  className='shrink-0'
+                >
+                  <Trash
+                    size={18}
+                    className='text-xl text-default-500 pointer-events-none flex-shrink-0'
                   />
-                </I18nProvider>
+                </Button>
               </div>
-              <Button
-                onPress={clearFilter}
-                isIconOnly
-                variant='flat'
-                radius='sm'
-                className='shrink-0'
-              >
-                <Trash
-                  size={18}
-                  className='text-xl text-default-500 pointer-events-none flex-shrink-0'
-                />
-              </Button>
+              <DateFilterPeriodButtons />
             </div>
-            <DateFilterPeriodButtons />
           </div>
-        </div>
-      </PopoverContent>
-    </Popover>
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 }
