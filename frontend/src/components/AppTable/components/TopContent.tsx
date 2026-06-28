b46e8ba3286afaabefa510a18a3a6a3e2f06bd54
Arviixzuh <arviixzuh@gmail.com>
2026-06-28 17:59:44 -0400

    style: 🌈 improve layout and spacing in TopContent component for better responsiveness

--- frontend/src/components/AppTable/components/TopContent.tsx
+++ frontend/src/components/AppTable/components/TopContent.tsx
@@ -2,9 +2,9 @@ import { Plus } from 'lucide-react'
 import { Button } from '@heroui/button'
 import { Searchbar } from './Searchbar'
 import { useDispatch } from 'react-redux'
+import { ChipFilterGroup } from './FilterBy'
 import { toggleAddItemModal } from '@/features/appTableSlice'
 import { FilterByDatePicker } from './FilterByDate'
-import { ChipFilterGroup } from './FilterBy'
 
 interface TopContentProps {
   hiddeAdd?: boolean
@@ -22,14 +22,16 @@ export const TopContent = ({
   const dispatch = useDispatch()
 
   return (
-    <div className='flex flex-col sm:flex-row gap-3 w-full mb-4 justify-between items-stretch sm:items-center'>
-      <div className='flex flex-wrap gap-3 items-center w-full sm:flex-1'>
-        <div className='flex gap-3 w-full max-w-none sm:max-w-lg shrink-0'>
+    <div className='flex flex-col sm:flex-row gap-4 w-full mb-4 justify-between items-stretch md:items-center'>
+      <div className='flex flex-row gap-3 items-center w-full lg:flex-1'>
+        <div className='w-full sm:w-80'>
           <Searchbar searchbarPlaceholder={searchbarPlaceholder} />
+        </div>
+        <div className='flex items-center gap-3'>
           <ChipFilterGroup />
           {filterByDate && <FilterByDatePicker />}
         </div>
-        {topContentExtension && topContentExtension}
+        {topContentExtension && <div className='flex-1 min-w-[150px]'>{topContentExtension}</div>}
       </div>
       {!hiddeAdd && (
         <div className='flex items-center justify-end shrink-0'>
