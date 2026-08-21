96e659fb0fdf04b8dc45896f130074ce9c07e6aa
Arviixzuh <arviixzuh@gmail.com>
2026-08-21 09:45:00 -0400

    feat: ✨ integrate VerificationCodeApplicationModule and EmailService into AuthModule

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { PrismaModule } from '@/prisma/prisma.module'
+import { EmailService } from '@/common/utils/mail-sender.util'
 import { AuthController } from './auth.controller'
+import { VerificationCodeApplicationModule } from '../verificationCode/application/verificationCode.application.module'
 
 @Module({
-  imports: [PrismaModule],
-  providers: [AuthService],
+  imports: [PrismaModule, VerificationCodeApplicationModule],
+  providers: [EmailService, AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
