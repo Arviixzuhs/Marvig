551da92e40991e226f69ae031111e4ded3efeeb0
Arviixzuh <arviixzuh@gmail.com>
2026-08-24 11:10:00 -0400

    feat: ✨ add ConfirmSigningDto for user confirmation with validation rules

---
+++ backend/src/modules/auth/dto/confirm-signin.dto.ts
@@ -0,0 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator'
+
+export class ConfirmSigningDto  {
+    @ApiProperty({
+        example: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2Vy',
+        description: 'Token de confirmación',
+    })
+    @IsNotEmpty({ message: 'El token es obligatorio.' })
+    @IsString()
+    token: string
+
+    @ApiProperty({
+        example: 'contraseñaSegura123!',
+        description: 'La contraseña del nuevo usuario.',
+    })
+    @IsNotEmpty({ message: 'La contraseña no debe estar vacía.' })
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres.' })
+    @Matches(/(?=.*[0-9])/, { message: 'La contraseña debe contener al menos un número.' })
+    @Matches(/(?=.*[!@#$%^&*(),.?":{}|<>])/, {
+        message: 'La contraseña debe contener al menos un símbolo especial.',
+    })
+    password: string
+
+    @ApiProperty({
+        example: 'contraseñaSegura123!',
+        description: 'La contraseña del nuevo usuario.',
+    })
+    @IsNotEmpty({ message: 'La contraseña no debe estar vacía.' })
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres.' })
+    @Matches(/(?=.*[0-9])/, { message: 'La contraseña debe contener al menos un número.' })
+    @Matches(/(?=.*[!@#$%^&*(),.?":{}|<>])/, {
+        message: 'La contraseña debe contener al menos un símbolo especial.',
+    })
+    repeatPassword: string
+}
