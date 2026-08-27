20f999a3725e7e8be852767919cfdb787c215bc1
Arviixzuh <arviixzuh@gmail.com>
2026-08-27 13:15:00 -0400

    feat: ✨ add public access decorator to create and validate verification code mutations

--- backend/src/modules/verificationCode/infrastructure/graphql/resolvers/verificationCode.resolver.ts
+++ backend/src/modules/verificationCode/infrastructure/graphql/resolvers/verificationCode.resolver.ts
@@ -3,21 +3,24 @@ import { CreateVerificationCodeInput } from '@/modules/verificationCode/infrastr
 import { CreateVarificationCodeUseCase } from '@/modules/verificationCode/application/usecases/create-verification-code.usecase'
 import { ValidateVerificationCodeInput } from '@/modules/verificationCode/infrastructure/graphql/inputs/validate-verification-code.input'
 import { ValidateVarificationCodeUseCase } from '@/modules/verificationCode/application/usecases/validate-verification-code.usecase'
+import { Public } from '@/common/decorators/public.decorator'
 
 @Resolver()
 export class VerificationCodeResolver {
   constructor(
     private readonly createVarificationCodeUseCase: CreateVarificationCodeUseCase,
     private readonly validateVarificationCodeUseCase: ValidateVarificationCodeUseCase,
-  ) {}
+  ) { }
 
   @Mutation(() => String)
+  @Public()
   async createVerificationCode(@Args('data') data: CreateVerificationCodeInput): Promise<String> {
     this.createVarificationCodeUseCase.execute(data.type, data.email)
     return 'Código de verificación creado exitosamente.'
   }
 
   @Mutation(() => Boolean)
+  @Public()
   async validateVerificationCode(
     @Args('data') data: ValidateVerificationCodeInput,
   ): Promise<boolean> {
