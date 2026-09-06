4fc1de88114a5012199fa88f15fdd9ccf895a262
Arviixzuh <arviixzuh@gmail.com>
2026-09-06 17:32:18 -0400

    feat: ✨ refactor auth interfaces for improved structure and clarity

--- frontend/src/modules/auth/services/interfaces.ts
+++ frontend/src/modules/auth/services/interfaces.ts
@@ -1,3 +1,4 @@
+
 export interface ReqChangePasswordByPin {
   pin: string
   email: string
@@ -5,15 +6,26 @@ export interface ReqChangePasswordByPin {
   repeatNewPassword: string
 }
 
-export interface IAuthRegisterUser {
-  email: string
-  password: string
-  lastName: string
+export interface IAuthSigning {
   name: string
+  email: string
+  lastName: string
+}
+
+export interface IAuthConfirmSigning {
+  token: string
+  password: string
   repeatPassword: string
 }
 
 export interface IAuthLoginUser {
   email: string
   password: string
+}
+
+export interface IChangePasswordByCode {
+  code: string
+  newPassword: string
+  repeatNewPassword: string
+  email: string
 }
